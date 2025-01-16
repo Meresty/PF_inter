0b35bf4a2696306abe5ac1e9a76c7ea3a31360c7
Shari Palma <shariaimee@gmail.com>
2025-01-16 17:32:38 -0600

    basurita :p

--- app/src/main/java/com/example/pf_inter/contenidoNota.java
+++ app/src/main/java/com/example/pf_inter/contenidoNota.java
@@ -9,6 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class contenidoNota extends AppCompatActivity {
+    String titulo, contenido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
