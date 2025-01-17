da58074e07b319df3def7a6f862c9f03d7b83d0b
LethalDrop8 <yamil.tinoflores@gmail.com>
2025-01-16 22:17:53 -0600

    funciona

--- app/src/main/java/com/example/pf_inter/MainActivity.java
+++ app/src/main/java/com/example/pf_inter/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configurar la barra de herramientas personalizada
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
+        // Infla el menú desde el archivo XML
         getMenuInflater().inflate(R.menu.menu, menu);
 
-
+        // Asegurarse de que los ítems del menú siempre se muestren en la barra de acción
         for (int i = 0; i < menu.size(); i++) {
             MenuItem item = menu.getItem(i);
             item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
@@ -30,14 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        // Manejo de las opciones del menú
         if (item.getItemId() == R.id.ITEliminar) {
-
             Intent eliminarIntent = new Intent(this, Eliminar.class);
             startActivity(eliminarIntent);
             return true;
         } else if (item.getItemId() == R.id.ITAgregar) {
-
             Intent agregarIntent = new Intent(this, contenidoNota.class);
             startActivity(agregarIntent);
             return true;
