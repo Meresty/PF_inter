bb4c9df9420db80cc384004f9e4a07be71b84600
Shari Palma <shariaimee@gmail.com>
2025-01-16 20:45:04 -0600

    basurita :p

---
+++ app/src/main/java/POJO/nota.java
@@ -0,0 +1,25 @@
+package POJO;
+
+public class nota {
+
+    public String Titulo;
+    public String Contenido;
+
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getContenido() {
+        return Contenido;
+    }
+
+    public void setContenido(String contenido) {
+        Contenido = contenido;
+    }
+
+}
---
+++ app/src/main/java/global/info.java
@@ -0,0 +1,14 @@
+package global;
+
+import java.util.ArrayList;
+
+import POJO.nota;
+
+public class info {
+
+    public static final ArrayList<nota>
+            lista = new ArrayList<>();
+
+    public static final ArrayList<nota>
+            listaEliminar = new ArrayList<>();
+}
