931e7d51c3201fca595997e1e23b66e4aef25db3
LethalDrop8 <yamil.tinoflores@gmail.com>
2025-01-16 21:59:56 -0600

    main

--- app/src/main/java/com/example/pf_inter/MainActivity.java
+++ app/src/main/java/com/example/pf_inter/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.pf_inter;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,35 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    /
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.menu, menu);
+
+
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        }
+
         return true;
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.ITEliminar:
 
-                Intent eliminarIntent = new Intent(this, EliminarActivity.class);
-                startActivity(eliminarIntent);
-                return true;
+        if (item.getItemId() == R.id.ITEliminar) {
 
-            case R.id.ITAgregar:
+            Intent eliminarIntent = new Intent(this, Eliminar.class);
+            startActivity(eliminarIntent);
+            return true;
+        } else if (item.getItemId() == R.id.ITAgregar) {
 
-                Intent agregarIntent = new Intent(this, contenidoNota.class);
-                startActivity(agregarIntent);
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
+            Intent agregarIntent = new Intent(this, contenidoNota.class);
+            startActivity(agregarIntent);
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 }
